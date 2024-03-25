fca5bfb5823b378e96cd25e1ec0644305030285f
rohanmathewalex <rohanmathewalex6674@gmail.com>
2024-03-24 22:10:58 -0400

    react router dom and Layout implemented

--- jobhive/src/App.jsx
+++ jobhive/src/App.jsx
@@ -1,19 +1,18 @@
+ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
  import React from 'react'
- import Navbar from './components/Navbar';
- import Hero from './components/Hero';
- import HomeCards from './components/HomeCards';
- import JobListings from './components/JobListings';
- import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element= {<HomePage />}/>
+  </Route>
+  )
+ )
+ 
  const App = () => {
-   return (
-    <>
-    <Navbar />
-    <Hero   />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-    </> 
-   );
+   return <RouterProvider router={router} />
  }
  
  export default App
